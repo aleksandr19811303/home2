096b7ce5ae2d0561b1ca49f2999088683c2eb059
Aleksandr <shbragnikov@inbox.ru>
2023-02-22 23:11:15 +0300

    2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// программа принимает число N и выдает таблицу кубов ичсел от 1 до N
+Console.Write("введите число  ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i =1;
+while (i<=N)
+{
+   Console.WriteLine (i*i*i);
+   i++;
+}
