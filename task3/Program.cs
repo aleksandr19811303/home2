75af12cf32aa7046bc7b04c24a9248c5a23ce977
Aleksandr <shbragnikov@inbox.ru>
2023-02-22 23:35:45 +0300

    5

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 19 программа проверяет пятизначное число на полиндром
+Console.WriteLine("введите число");
+ string a = Console.ReadLine();
+ int b = a.Length;
+ if (b==5)
+ {
+    if (a[0]==a[4] && a[1]==a[3])
+    {
+        Console.WriteLine ("полиндром");
+    }
+    else
+    {
+      Console.WriteLine ("не полиндром"); 
+    }
+     }
+     else
+     {
+      Console.WriteLine ("ошибка");  
+     }
